2c896531688a50f08dcfccb5cc7d29efdc88fad4
Brad Graham <blg002@gmail.com>
2011-02-09 09:28:27 -0500

    updating nsm_addon_updater

--- system/expressionengine/third_party/nsm_addon_updater/config.php
+++ system/expressionengine/third_party/nsm_addon_updater/config.php
@@ -8,15 +8,15 @@
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
- * @link			http://expressionengine-addons.com/nsm-addon-updater
+ * @link			http://ee-garage.com/nsm-addon-updater
  */
 
 
 $config['name']				= 'NSM Addon Updater';
 $config['version']	 		= '1.0.1';
 
-$config['nsm_addon_updater']['versions_xml'] 			= 'http://expressionengine-addons.com/nsm-addon-updater/release-notes';
+$config['nsm_addon_updater']['versions_xml'] 			= 'http://ee-garage.com/nsm-addon-updater/release-notes/feed';
 $config['nsm_addon_updater']['custom_download_url'] 	= array('Nsm_addon_updater_acc', 'nsm_addon_updater_download_url');
 
 // Local test XML
-// $config['nsm_addon_updater']['versions_xml'] = 'http://local.expressionengine-addons.com/system/expressionengine/third_party/nsm_addon_updater/test_versions.xml';
+// $config['nsm_addon_updater']['versions_xml'] = 'http://local.ee-garage.com/system/expressionengine/third_party/nsm_addon_updater/test_versions.xml';
